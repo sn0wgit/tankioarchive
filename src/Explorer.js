dd49e04ad4650bc475c684256e70c1a443cf02a0
yaroslav.vyazmitin <yaroslav.vyazmitin@gmail.com>
2024-07-04 19:32:46 +0000

    уже работает!

--- src/Explorer.js
+++ src/Explorer.js
@@ -7,46 +7,38 @@ function Explorer() {
     const { t, i18n } = useTranslation();
     const location = useLocation();
     const { pathname } = location;
-
-    console.log(pathname);
+    i18n.loadNamespaces(pathname.replace("tankioarchive/", "").substring(1));
     const arrangement = pathname.replace("tankioarchive/", "tankioarchive/archive/")+"/arrangement.json"
-/*  fetch(arrangement).then(response => {
+    fetch(arrangement).then(response => {
         console.log(response);
         return response.json();
     }).then(data => {
-        data.content.forEach(element => {
+        console.log("forEach", data)
+        data.content.forEach((itemunit) => {
             let newrow = document.createElement("tr");
-            let name = document.createElement("td");
-            let type = document.createElement("td");
-            let actions = document.createElement("td");
-            if (element.type === "folder"){
-                type.innerHTML = '{t("explorer.folder")}';
-                actions.innerHTML = '<a href="'+element.url+'" class="folder"></a><a href="'+element.url+'" class="new_tab" target="_blank"></a>';
-            } else {
-                type.innerHTML = '{t("explorer.file")}';
-                actions.innerHTML = '<a href="'+element.url+'" class="new_tab" target="_blank"></a>';
+            let rowname = document.createElement("td");
+            let rowtype = document.createElement("td");
+            let rowactions = document.createElement("td");
+            rowname.innerText = t(itemunit.name, {ns: pathname.replace("tankioarchive/", "").substring(1)});
+            rowtype.innerText = t(itemunit.type);
+            if (itemunit.preview !== false) {
+                rowactions.innerHTML = `<a href=${itemunit.preview} class="preview"></a>`;
             }
-            if (element.preview !== false){
-                actions.innerHTML += '<a href="'+element.url+'" class="preview-btn" target=""></a>';
+            rowactions.innerHTML += `<a href=${itemunit.url} class="new_tab" target="_blank" rel="noreferrer"></a>`;
+            if (itemunit.type === 'explorer.folder') {
+                rowactions.innerHTML += `<a href=${itemunit.url} class="folder"></a>`;
             }
-            name.innerHTML = element.id;
-            newrow.appendChild(name);
-            newrow.appendChild(type);
-            newrow.appendChild(actions);
-            document.querySelector("tbody").appendChild(newrow)
-        });
+            else {
+                rowactions.innerHTML += `<a href=${itemunit.url} class="download" download="true"></a>`;
+            }
+            newrow.appendChild(rowname);
+            newrow.appendChild(rowtype);
+            newrow.appendChild(rowactions);
+            document.querySelector("tbody").appendChild(newrow);
+        })
     }).catch(err => {
         console.log("Error Reading data " + err);
-    });  */
-    /*
-    {Object.keys(data.content).map((unit) => (
-        <tr key={unit}>
-            <td>{t({unit.id})}</td>
-            <td>{data.content[unit]}</td>
-        </tr>
-    ))}
-    */
-    i18n.loadNamespaces(pathname.replace("tankioarchive/", ""));
+    });
     return (
         <section className="explorer">
             <div className="window">
